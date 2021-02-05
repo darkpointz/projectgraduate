ea90ccb7226a4da0d435baa0f63650ecfce62a0c
aodkorarit <korarit1@gmail.com>
2021-02-05 19:42:03 +0700

    5/2/2021

--- src/Page/class.js
+++ src/Page/class.js
@@ -5,16 +5,14 @@ import {
   Paper,
   Button,
   FormControl,
-  InputAdornment,
   Typography,
   Box,
-  TextField,
   Grid,
-  Dialog,
 } from "@material-ui/core";
 import { Add, Search } from "@material-ui/icons";
 import DialogCreateClass from "../Components/dialogCreateClass";
 import DialogCreateClassNew from "../Components/dialogCreateClassNew";
+import CardClass from "../Components/cardClass";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -76,6 +74,18 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+function createData(name, calories, fat, carbs) {
+  return { name, calories, fat, carbs };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24),
+  createData("Ice cream sandwich", 237, 9.0, 37),
+  createData("Eclair", 262, 16.0, 24),
+  createData("Cupcake", 305, 3.7, 67),
+  createData("Gingerbread", 356, 16.0, 49),
+];
+
 export default function Class() {
   const classes = useStyles();
   const [openCreateClass, setOpenCreateClass] = useState(false);
@@ -115,9 +125,9 @@ export default function Class() {
         </Grid>
 
         <Grid container item xs={12}>
-          <Paper className={classes.paper}>
-            <Typography className={classes.typotitlePaper}>Class</Typography>
-          </Paper>
+          {room.map((room, index) => {
+            return <CardClass room={room} />;
+          })}
         </Grid>
         <DialogCreateClass
           open={openCreateClass}
@@ -132,3 +142,10 @@ export default function Class() {
     </div>
   );
 }
+//----
+
+//   <Grid container item xs={12}>
+//   <Paper className={classes.paper}>
+//     <Typography className={classes.typotitlePaper}>Class</Typography>
+//   </Paper>
+// </Grid>
