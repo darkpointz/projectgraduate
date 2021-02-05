8fe710b67b698931794ad57d0afc25a173d0429b
Korarit Sukcharoenthong <korarit1@gmail.com>
2021-02-05 22:24:08 +0700

    5/02/2021

--- src/Components/cardClass.js
+++ src/Components/cardClass.js
@@ -15,11 +15,8 @@ import {
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
-  },
-  bullet: {
-    display: "inline-block",
-    margin: "0 2px",
-    transform: "scale(0.8)",
+    marginRight: "18px",
+    borderRadius: "16px"
   },
   title: {
     fontSize: 14,
@@ -39,7 +36,7 @@ export default function CardClass(props) {
           color="textSecondary"
           gutterBottom
         >
-          Word of the Day
+          {room.room}
         </Typography>
         <Typography variant="h5" component="h2"></Typography>
         <Typography className={classes.pos} color="textSecondary">
@@ -57,3 +54,27 @@ export default function CardClass(props) {
     </Card>
   );
 }
+//---
+{/* <Card className={classes.root}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          {room.room}
+        </Typography>
+        <Typography variant="h5" component="h2"></Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography variant="body2" component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card> */}
